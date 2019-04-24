4640636eadb8e71040a2b8bbeefca21e70f4ef07
ikhda <ikhdamuhammad@gmail.com>
2019-04-24 20:42:48 +0700

    add pug

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const http = require("http");
+const path = require("path");
+
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next(); // allows you to go to the another middleware
-});
+app.set("view engine", "pug");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>Hello from Express!</h1>");
+  res.status(404).render("404", { pageTitle: "Page not Found" });
 });
 
-const server = http.createServer(app);
-server.listen(3000);
+app.listen(3000);
