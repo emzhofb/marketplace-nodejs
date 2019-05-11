77e38bba20fe347c60384eaa48cc39caeadd46f2
ikhda <ikhdamuhammad@gmail.com>
2019-05-11 18:41:14 +0700

    initial commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key: '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'
+  }
+}));
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -83,6 +91,15 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
           res.redirect('/login');
+          return transporter.sendMail({
+            to: email,
+            from: 'shop@emzhofb.github.io',
+            subject: 'SignUp Succeeded!',
+            html: '<h1>You successfully signed up!</h1>'
+          });
+        })
+        .catch(err => {
+          console.log(err);
         });
     })
     .catch(err => {
