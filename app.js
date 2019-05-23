c545819613bb881c61d17644ba4b01823e2311aa
ikhda <ikhdamuhammad@gmail.com>
2019-05-23 21:53:35 +0700

    add socket io

--- app.js
+++ app.js
@@ -60,8 +60,14 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect('mongodb+srv://ikhda:wildani@cluster0-v8mzg.mongodb.net/messages')
+  .connect(
+    'mongodb+srv://ikhda:wildani@cluster0-v8mzg.mongodb.net/messages?retryWrites=true'
+  )
   .then(result => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require('./socket').init(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    });
   })
   .catch(err => console.log(err));
